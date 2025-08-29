765a43f486b5082534123f6ac0f91f2effc9dabd
RestfulApiBoard <pd8459@gmail.com>
2025-08-29 13:08:00 +0900

    fix: Resolve PostgreSQL reserved word conflict by renaming user table

--- src/main/java/com/example/puppytalk/User/User.java
+++ src/main/java/com/example/puppytalk/User/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
 //@Where(clause = "is_deleted = false")
 public class User {
 
