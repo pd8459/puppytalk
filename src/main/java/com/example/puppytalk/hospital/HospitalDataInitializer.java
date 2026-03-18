96317ab432b2eccce211ea3950e7d22b8e276a81
RestfulApiBoard <pd8459@gmail.com>
2026-03-18 15:36:09 +0900

    fix: Changed Flowerpot to CommandLineRunner (Resolved table creation meeting)

--- src/main/java/com/example/puppytalk/hospital/HospitalDataInitializer.java
+++ src/main/java/com/example/puppytalk/hospital/HospitalDataInitializer.java
@@ -1,23 +1,22 @@
 package com.example.puppytalk.hospital;
 
 import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
-import java.io.IOException;
+
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 @Component
 @RequiredArgsConstructor
-public class HospitalDataInitializer {
+public class HospitalDataInitializer implements CommandLineRunner {
 
     private final AnimalHospitalRepository hospitalRepository;
 
-    @PostConstruct
-    public void loadData() throws CsvValidationException, IOException {
+    @Override
+    public void run(String... args) throws Exception {
         if (hospitalRepository.count() > 0) {
             return;
         }
--- src/main/java/com/example/puppytalk/playground/DataInitializer.java
+++ src/main/java/com/example/puppytalk/playground/DataInitializer.java
@@ -1,21 +1,22 @@
 package com.example.puppytalk.playground;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 @Component
 @RequiredArgsConstructor
-public class DataInitializer {
+public class DataInitializer implements CommandLineRunner {
 
     private final PlaygroundRepository playgroundRepository;
 
-    @PostConstruct
-    public void loadData() {
+    @Override
+    public void run(String... args) throws Exception {
         if (playgroundRepository.count() > 0) {
             return;
         }
