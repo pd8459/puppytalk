254a4722f1f4af11c8493f554a2e34d3594c5ddc
RestfulApiBoard <pd8459@gmail.com>
2026-04-13 14:09:38 +0900

    feat: Refactoring - Removal of Lens Upload and Complete Switching to S3 Upload

--- src/main/java/com/example/puppytalk/image/ImageController.java
+++ src/main/java/com/example/puppytalk/image/ImageController.java
@@ -1,6 +1,5 @@
 package com.example.puppytalk.image;
 
-import com.example.puppytalk.FileUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
